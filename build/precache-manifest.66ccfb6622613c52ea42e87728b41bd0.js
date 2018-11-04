546b080461f63da8af966193ad053d37eb429234
Zuhair Taha <admin@tahasoft.com>
2018-11-04 08:06:11 +0100

    add github pages deploy

---
+++ build/precache-manifest.66ccfb6622613c52ea42e87728b41bd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "606aaa2593d0a0dbda9f",
+    "url": "/react-week2-todo/static/js/runtime~main.606aaa25.js"
+  },
+  {
+    "revision": "939d753a8b88ad342b53",
+    "url": "/react-week2-todo/static/js/main.939d753a.chunk.js"
+  },
+  {
+    "revision": "20ce0175aa1d43a728d2",
+    "url": "/react-week2-todo/static/js/1.20ce0175.chunk.js"
+  },
+  {
+    "revision": "939d753a8b88ad342b53",
+    "url": "/react-week2-todo/static/css/main.09e6ce7b.chunk.css"
+  },
+  {
+    "revision": "8594b65c8344c0992d06afef08dca501",
+    "url": "/react-week2-todo/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.623cc20f5d572edb6a09d191e774ba13.js"
+  "/react-week2-todo/precache-manifest.66ccfb6622613c52ea42e87728b41bd0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-week2-todo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
