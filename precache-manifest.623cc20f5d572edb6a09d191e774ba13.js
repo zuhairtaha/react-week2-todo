a9481826cdbfa20a07254fca3096f453a938fd6b
Zuhair Taha <admin@tahasoft.com>
2018-11-04 07:59:05 +0100

    Updates

---
+++ precache-manifest.623cc20f5d572edb6a09d191e774ba13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae5ade2020fa62ca94ac",
+    "url": "/static/js/main.ae5ade20.chunk.js"
+  },
+  {
+    "revision": "20ce0175aa1d43a728d2",
+    "url": "/static/js/1.20ce0175.chunk.js"
+  },
+  {
+    "revision": "ae5ade2020fa62ca94ac",
+    "url": "/static/css/main.09e6ce7b.chunk.css"
+  },
+  {
+    "revision": "5f5dbb80229853bde76e043486daed0f",
+    "url": "/index.html"
+  }
+];
